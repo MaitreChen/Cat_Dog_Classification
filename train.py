9a0e7bf3f5615f7c6b9173854ec1c3b5ab37ec21
thebin <1974911747@qq.com>
2023-05-26 16:04:34 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,17 +1,47 @@
 # IMPORT PACKAGE
 from prettytable import PrettyTable
+import datetime
+import os
+
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import tensorflow as tf
-import datetime
+
+# Create model dir
+MODEL_PATH = './model_data'
+if not os.path.exists(MODEL_PATH):
+    os.makedirs(MODEL_PATH)
 
 # Define image attribute
 IMG_HEIGHT = 150
 IMG_WIDTH = 150
-NUM_CLASSES = 60
+NUM_CLASSES = 2
+
+# Define hyper-parameters
+BATCH_SIZE = 64
+EPOCHS = 40
+lr = 0.001
+optimizer = Adam(learning_rate=lr)
+
+# Add to table
+# Create the table object, name, and alignment
+table = PrettyTable(['Hyper-Parameters & data infos', 'Value'])
+table.align['Hyper-Parameters & data infos'] = 'l'
+table.align['Value'] = 'r'
+
+table.add_row(['BATCH_SIZE', BATCH_SIZE])
+table.add_row(['EPOCHS', EPOCHS])
+table.add_row(['LR', lr])
+table.add_row(['optimizer', optimizer])
+table.add_row(['', ''])
+
+table.add_row(['IMG_HEIGHT', IMG_HEIGHT])
+table.add_row(['IMG_WIDTH ', IMG_WIDTH])
+table.add_row(['NUM_CLASSES ', NUM_CLASSES])
+print(table)
 
 # Build model
 model = Sequential([
@@ -26,30 +56,6 @@ model = Sequential([
     Dense(1, activation='sigmoid')
 ])
 
-# Define hyper-parameters
-BATCH_SIZE = 64
-EPOCHS = 40
-lr = 0.0001
-optimizer = Adam(learning_rate=lr)
-
-# Create the table object, name, and alignment
-table = PrettyTable(['Hyper-Parameters & data infos', 'Value'])
-table.align['Hyper-Parameters & data infos'] = 'l'
-table.align['Value'] = 'r'
-
-# Add to table
-table.add_row(['BATCH_SIZE', BATCH_SIZE])
-table.add_row(['EPOCHS', EPOCHS])
-table.add_row(['LR', lr])
-table.add_row(['optimizer', optimizer])
-table.add_row(['', ''])
-
-table.add_row(['IMG_HEIGHT', IMG_HEIGHT])
-table.add_row(['IMG_WIDTH ', IMG_WIDTH])
-table.add_row(['NUM_CLASSES ', NUM_CLASSES])
-print(table)
-
-
 # Compile model
 model.compile(optimizer=optimizer, loss='binary_crossentropy',
               metrics=['accuracy'])
@@ -57,7 +63,7 @@ log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(
     log_dir=log_dir, histogram_freq=1)
 
-
+# Load dataset
 train_data_gen = ImageDataGenerator(rescale=1. / 255,
                                     shear_range=0.1,
                                     zoom_range=0.1,
@@ -88,7 +94,8 @@ epoch_steps = len(train_set)
 val_epoch_steps = len(val_set)
 
 early_stop = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode='min')
-best_model = ModelCheckpoint('./model_data/best_model.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+best_model = ModelCheckpoint(f'{MODEL_PATH}/best_model.h5', monitor='val_loss', verbose=1, save_best_only=True,
+                             mode='min')
 
 # Train model with callbacks
 history = model.fit(
@@ -99,19 +106,6 @@ history = model.fit(
     validation_steps=val_epoch_steps,
     callbacks=[tensorboard_callback, early_stop, best_model]
 )
-
-# # Train model
-# model.fit(
-#     train_set,
-#     steps_per_epoch=epoch_steps,
-#     epochs=EPOCHS,
-#     validation_data=val_set,
-#     validation_steps=val_epoch_steps,
-#     callbacks=[tensorboard_callback]
-# )
-
-# # Save model
-# model.save(f'./model_data/model_lr{lr}.h5')
 
 # Evaluate model
 train_info = model.evaluate(train_set)
